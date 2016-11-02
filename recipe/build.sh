5cb1225d34a6702d988a40a747cef57b1f00997b
ocefpaf <ocefpaf@gmail.com>
2016-11-02 16:10:42 -0300

    Update to 1.6.26

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,9 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 
-autoreconf -vif
-
 ./configure --prefix=$PREFIX \
             --with-zlib-prefix=$PREFIX
 
 make
 make check
 make install
-
-cp $RECIPE_DIR/libpng-LICENSE.txt $SRC_DIR/libpng-LICENSE.txt
