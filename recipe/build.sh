cd73404722d45cfbdf3ebee33590b6b55421540c
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-02 20:44:44 -0300

    remove .la

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,3 +9,6 @@ export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
