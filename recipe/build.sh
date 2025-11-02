86d1d89d8af5a336e94e1c5e74727eee26cd02a2
Mark Harfouche <mark.harfouche@gmail.com>
2025-11-01 21:38:47 -0400

    Use zlib-ng instead of zlib

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-
-./configure --prefix=$PREFIX \
-            --with-zlib-prefix=$PREFIX
+./configure --prefix=$PREFIX
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
