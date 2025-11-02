95e784a5f568d6737450e38b70f0807b21612619
Mark Harfouche <mark.harfouche@gmail.com>
2025-11-01 21:53:38 -0400

    Add correct test image

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,12 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
+# hmaarrfk - 2025/11/01
+# We at conda-forge changed the zlib implementation to zlib-ng
+# this changes the bit-for-bit results in the png test image
+# So I (hmaarrfk) generated it with zlib-ng and saved it
+cp ${RECIPE_DIR}/pngtest.png ./pngtest.png
+
 ./configure --prefix=$PREFIX
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
